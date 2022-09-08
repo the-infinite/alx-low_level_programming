a5c6041e4ce08daf8b9252f26cc10bf1b971ec34
the-infinite <toby.johnson.precious@gmail.com>
2022-09-08 11:04:45 +0100

    Made betty-compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- * This is my function main. 
+ * main - This is my function main.
+ *
+ * Description: Print out a message to the user.
+ *
+ * Return: 0 everytime. So it is successful.
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
