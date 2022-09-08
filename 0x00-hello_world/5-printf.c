23bc488392919d9eebea68e61c2875f9dc3aa054
the-infinite <toby.johnson.precious@gmail.com>
2022-09-08 11:08:26 +0100

    Next

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  The entry point of this program
+*
+* Description: This is meant to tell the user something
+*
+* Return: 0 everytime
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
