c2650804e8b25367ba7b92707def8b9c41bc23b1
the-infinite <toby.johnson.precious@gmail.com>
2022-09-08 11:00:08 +0100

    This is persistent commitment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * This is my function main. 
+ */
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
