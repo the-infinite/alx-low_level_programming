12b8e35a8a0b1e55e715076caa0b09fbe14cbeba
the-infinite <toby.johnson.precious@gmail.com>
2022-09-08 11:27:03 +0100

    Final commit for this project.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point, always, ways na ways, and the way of ways.
+* Description: Prints some quote to the user though.
+* Return: 0 everytime.
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+	Dora Korpar 2015-10-19\n");
+	return (0);
+}
