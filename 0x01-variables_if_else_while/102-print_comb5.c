6ffc297c263749720773526b711a26332241d5c6
the-infinite <toby.johnson.precious@gmail.com>
2022-09-09 13:56:07 +0100

    Final commit for the optional tasks

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Prints all distinct 2 digit numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++secondDigit;
+		}
+		++firstDigit;
+	}
+	putchar('\n');
+
+	return (0);
+}
