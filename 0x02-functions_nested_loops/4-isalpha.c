a84c1246fe45092ef9fca6011f5479c21122b577
the-infinite <toby.johnson.precious@gmail.com>
2022-09-13 18:07:34 +0100

    Next is meaning

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _isalpha - checks if character is a letter
@@ -11,8 +11,8 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) ||
+	    (c >= 65 && c >= 90))
 		return (1);
 	return (0);
 }
