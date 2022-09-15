197c228ecb9a0929c3d70bb9163692ac3bc694f9
the-infinite <toby.johnson.precious@gmail.com>
2022-09-15 12:03:28 +0100

    First and final one for this project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return results
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The Repository for my ALX project advancing on functions and nested lops in  C
