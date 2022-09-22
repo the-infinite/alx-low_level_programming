e9df0c14cae100330c7d0361193a7cfff9570203
the-infinite <toby.johnson.precious@gmail.com>
2022-09-22 11:51:01 +0100

    Fixed: Fixed 100-rot13 and 101-print_number

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,4 @@
 /**
-* rot - calculates the rotated value of a single character.
-* @c: The character to rotate.
-* Return: The rotated value of the character.
-*/
-char rot(char c)
-{
-	char *rotd = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM\0";
-	char *norm = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
-
-	for (int i = 0; norm[i] != 0; i++)
-	{
-		if (norm[i] == c) /* If we have found a match */
-		{
-			return (rotd[i]); /*Return the rotated character*/
-		}
-	}
-
-	return (c); /* Found no match? Just return the character! */
-}
-
-/**
 * rot13 - Rotates each character in a string by 13.
 *
 * @s: The string to encode in ROT-13
@@ -28,10 +7,25 @@ char rot(char c)
 */
 char *rot13(char *s)
 {
-	for (int i = 0; s[i] != 0; i++)
+	char rotd[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM\0";
+	char norm[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
+
+	for (int i = 0; s[i] != 0 && s[i] != '\n'; i++)
 	{
-		s[i] = rot(s[i]); /* Just rotate */
+		char c = s[i];
+		char rot = c;
+
+		for (int j = 0; j < 52; j++)
+		{
+			if (norm[j] == c) /* If we have found a match */
+			{
+				rot = rotd[j]; /*Return the rotated character*/
+			}
+		}
+
+		s[i] = rot; /*Rotate this*/
 	}
+
 
 	return (s);
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
-* raise - Raises 10 by a set value.
-*
-* @n: Raise 10 to power n
-* Return: 10 raised to @n
-*/
-int raise(int n)
-{
-	int ret = 1;
-	int i = n;
-
-	while (i > 0)
-	{
-		ret = ret * 10;
-		i--;
-	}
-
-	return (ret);
-}
-
-/**
 * print_number - Prints the string value of a number.
 * @n: The number we wish to print.
 * Return: Does not return a value.
@@ -33,20 +13,14 @@ void print_number(int n)
 	if (n < 0) /* If this number is negative */
 	{
 		_putchar(45); /* Start with the minus sign */
-	}
-
-	if (value < 0)
-	{
 		value = value * -1; /* Absolute value only */
 	}
-
 	int temp = value; /*Used to calculate the number of digits temporarily */
 
 	while (1) /* Count the number of digits in this number */
 	{
 		temp /= 10;
 		digits++;
-
 		if (temp < 1)
 		{
 			break;
@@ -55,8 +29,15 @@ void print_number(int n)
 
 	while (1)
 	{
-		/* Reduce digits before use otherwise it's positionally be +1 wrong */
-		char c = ((value / raise(--digits)) % 10) + 48;
+		int ret = 1;
+		int i = --digits;
+
+		while (i > 0)
+		{
+			ret = ret * 10;
+			i--;
+		}
+		char c = ((value / ret % 10) + 48;
 
 		_putchar(c);
 		if (digits < 1)
