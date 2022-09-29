df10fdea42e09dff1459710ea1525826f6e38089
the-infinite <toby.johnson.precious@gmail.com>
2022-09-29 05:51:46 +0100

    Trying to fix the last number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
-* is_prime_number - Checks if a value is a prime number
-* @n: The number to check for prime nature.
-* Return: 1 when the number is prime and 0 otherwise.
+* prime_helper - THe helper function for checking if a number is prime.
+* @n: The number.
+* @index: The index to start from.
+* Return: 1 when true and 0 otherwise.
 */
-int is_prime_number(int n)
+int prime_helper(int n, int index)
 {
+	int i, bound;
+
+	bound = n / 2;
+
 	if (n < 2)
 	{
 		return 0;
@@ -17,20 +22,25 @@ int is_prime_number(int n)
 		return 1;
 	}
 
-	else if (n % 2 != 0)
+	else if (n % index != 0)
 	{
 		return 0;
 	}
-	
-	else if ((n + 1) % 2 == 0)
+
+	else if (index <= n / 2)
 	{
-		return is_prime_number(n / 3);
+		return 1;
 	}
 
-	else
-	{
-		return is_prime_number(n / 2);
-	}
+	return prime_helper(n, index+1)
+}
 
-	return 1;
+/**
+* is_prime_number - Checks if a value is a prime number
+* @n: The number to check for prime nature.
+* Return: 1 when the number is prime and 0 otherwise.
+*/
+int is_prime_number(int n)
+{
+	return prime_helper(n, 2);
 }
