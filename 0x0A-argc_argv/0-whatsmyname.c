e94ab5cd7054a38e2b676ace1b2cbdb17219935e
the-infinite <toby.johnson.precious@gmail.com>
2022-09-30 22:04:42 +0100

    First and last for the day

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name and path to its executable.
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: 0.
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+The Repository for my ALX project on main functions, arguments and the likes in C
