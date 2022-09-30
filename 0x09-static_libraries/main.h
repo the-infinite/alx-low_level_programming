a80fc7421e4f711eaf7d17b7219f5082e5c186f0
the-infinite <toby.johnson.precious@gmail.com>
2022-09-30 22:18:03 +0100

    First and last for the day

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H *func _atoi is incomplete*/
+#endif /* MAIN_H _atoi is incomplete*/
