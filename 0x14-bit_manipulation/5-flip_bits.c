590b907896122710c01bbad128d54f42aef6952a
the-infinite <toby.johnson.precious@gmail.com>
2022-10-27 23:43:13 +0100

    Fixed: Removed debug helpers

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -33,32 +33,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	/*xor n and b*/
 	diff = n ^ m;
 
-	#ifdef DEBUG
-	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
-	print_bin(n);
-	printf(" ");
-	print_bin(m);
-	printf(" diff is %ld and in binary is ", diff);
-	print_bin(diff);
-	printf("\n");
-	#endif
-
 	/*keep shifting diff to right*/
 	do {
-		#ifdef DEBUG
-		printf("diff %ld before right shift ", diff);
-		print_bin(diff);
-		printf("\n");
-		#endif
-
 		diff_bits += (diff & 1);
 		diff >>= 1;
-
-		#ifdef DEBUG
-		printf("diff %ld after right shift ", diff);
-		print_bin(diff);
-		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
-		#endif
 	} while (diff > 0);
 
 	return (diff_bits);
